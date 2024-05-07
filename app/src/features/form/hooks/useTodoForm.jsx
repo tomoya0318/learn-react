2afcb132884ac0052990f25a656697ab7329aa95
tomoya-n <s266227@wakayama-u.ac.jp>
2024-05-07 17:09:49 +0900

    add exception handling

--- app/src/features/form/hooks/useTodoForm.jsx
+++ app/src/features/form/hooks/useTodoForm.jsx
@@ -2,19 +2,23 @@ import { useState } from "react";
 
 const useTodoForm = ({ createTodo }) => {
     const [enteredTodo, setEnteredTodo] = useState("");
+    const [error, setError] = useState("");
 
     const addTodo = (e) => {
         e.preventDefault();
 
-        const newTodo = {
-            id: Math.floor(Math.random() * 1e5),
-            content: enteredTodo,
+        if (enteredTodo === ''){
+            setError('This field cannot empty.');
+        }else{
+            const newTodo = {
+                id: Math.floor(Math.random() * 1e5),
+                content: enteredTodo,
+            }
+            createTodo(newTodo);
+            setEnteredTodo("");
         }
-
-        createTodo(newTodo);
-        setEnteredTodo("");
     }
-    return { enteredTodo, setEnteredTodo, addTodo };
+    return { enteredTodo, setEnteredTodo, addTodo, error, setError };
 
 };
 
--- app/src/features/form/update-form.jsx
+++ app/src/features/form/update-form.jsx
@@ -1,15 +1,27 @@
 import useTodoForm from "./hooks/useTodoForm";
 const UpdateForm = ({ createTodo }) => {
-  const { enteredTodo, setEnteredTodo, addTodo } = useTodoForm({ createTodo });
+  const { enteredTodo, setEnteredTodo, addTodo, error, setError  } = useTodoForm({ createTodo });
+  
+  const handleChnage = (e) =>{
+    const value = e.target.value;
+    setEnteredTodo(value);
+    if (e.target.value === '') {
+      setError('This field cannot be empty.');
+    } else {
+        setError('');
+    }
+  };
+
   return (
     <div>
       <form onSubmit={addTodo}>
         <input
           type="text"
           value={enteredTodo}
-          onChange={(e) => setEnteredTodo(e.target.value)}
+          onChange={handleChnage}
         />
         <button>追加</button>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </form>
     </div>
   );
